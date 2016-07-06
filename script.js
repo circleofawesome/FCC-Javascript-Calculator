164575b739d70ade297310bda3012b6555908e17
Samiul Anwar <samiul.anwar@gmail.com>
2016-07-05 23:13:35 -0400

    calculator is functional,adding styles

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-//takes a form input as a string and returns the result
-function calculateString(str){
-	return eval(str);
-}
-
 var input="";
+
+//takes a form input as a string and returns the result
+function inputEquals(){
+	var ans=eval(input);
+	console.log(ans);
+	if(ans===undefined){
+		document.getElementById("inputField").innerHTML="0";
+		input="";
+	}
+	else{
+		document.getElementById("inputField").innerHTML=ans;
+		input="";
+	}
+	//document.getElementById("inputField").innerHTML=ans;
+}
 
 function inputUpdate(num){
 	//return input+=num;
-	console.log(input+=num);
+	var update=input+=num;
+	console.log(update);
+	document.getElementById("inputField").innerHTML=update;
 }
 
+function allClear(){
+	input="";
+	console.log(input);
+	document.getElementById("inputField").innerHTML="0";
+}
